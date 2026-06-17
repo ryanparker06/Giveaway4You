e9f662e827ef13b4337d01310538b32a42897c54
Ryan Parker <ryanparker3369@gmail.com>
2026-06-17 21:15:52 +0100

    Always send giveaway end announcement

--- src/utils/endGiveaway.js
+++ src/utils/endGiveaway.js
@@ -234,7 +234,6 @@ module.exports = async function endGiveaway(
 
       `Participants: ${participantCount}\n` +
       `Winner(s): ${winnersText}\n` +
-      'Prize: ${prize}\n' +
       `Ended: <t:${endTimestamp}:R>\n` +
       `Hosted By: <@${giveaway.hostedBy}>` +
       bonusRolesText
