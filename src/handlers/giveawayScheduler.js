16e67259d2c65e967ab3006f901a303d4993f328
Ryan Parker <ryanparker3369@gmail.com>
2026-06-17 19:41:36 +0100

    Fix giveaway prize title

--- src/handlers/giveawayScheduler.js
+++ src/handlers/giveawayScheduler.js
@@ -52,7 +52,7 @@ for (const giveaway of scheduledGiveaways) {
    // Title
    container[0].components[0] = {
     type: 10, // Text Display
-    content: `# 🎁 ${prize}`
+    content: `# 🎁 ${giveaway.prize}`
   };
 
     // Separator below title
